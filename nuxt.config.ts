8be033aade78ee4e4fc08cb7f8dd93c1fa2f3ed8
Artem <myartoym@gmail.com>
2026-02-05 00:26:48 +0300

    first

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  
+  // SSG конфигурация для GitHub Pages
+  ssr: false, // Отключаем SSR для статической генерации
+  
+  // Настройка для GitHub Pages
+  // Для репозитория в корне GitHub Pages (username.github.io) установите GITHUB_PAGES_BASE_URL=/
+  // Для репозитория в подпапке будет автоматически использоваться /имя-репозитория/
+  app: {
+    baseURL: process.env.GITHUB_PAGES_BASE_URL 
+      || (process.env.GITHUB_REPOSITORY_NAME ? `/${process.env.GITHUB_REPOSITORY_NAME}/` : '/'),
+    buildAssetsDir: '/_nuxt/'
+  },
+  
+  nitro: {
+    prerender: {
+      routes: ['/']
+    }
+  }
+})
