26ca05163e6ae271f228d5017ae2c836a514ee6c
Artem <myartoym@gmail.com>
2026-02-12 00:15:39 +0300

    test2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,8 +15,6 @@ export default defineNuxtConfig({
       // Явно указать маршруты для пререндеринга (опционально)
       routes: [
         '/',
-        '/lash',
-        '/brows-lamination'
       ]
     },
     // Минификация для продакшена
